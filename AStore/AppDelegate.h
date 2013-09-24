49b41d2164077be23a6aa75f60a1eb73cc1a22c6
Carl-XPG <787887347@qq.com>
2013-09-24 23:06:30 +0800

    Initial Commit

---
+++ AStore/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  AStore
+//
+//  Created by Carl on 13-9-24.
+//  Copyright (c) 2013年 carl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ AStore/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  AStore
+//
+//  Created by Carl on 13-9-24.
+//  Copyright (c) 2013年 carl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end

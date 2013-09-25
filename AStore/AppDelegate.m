6826cd5657d109f7400bf9982e4c0fea01dd58a9
Carl-XPG <787887347@qq.com>
2013-09-25 15:54:49 +0800

    add picture & group

--- AStore/AppDelegate.m
+++ AStore/AppDelegate.m
@@ -8,7 +8,7 @@
 
 #import "AppDelegate.h"
 
-#import "ViewController.h"
+
 
 @implementation AppDelegate
 
@@ -16,8 +16,7 @@
 {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     // Override point for customization after application launch.
-    self.viewController = [[ViewController alloc] initWithNibName:@"ViewController" bundle:nil];
-    self.window.rootViewController = self.viewController;
+
     [self.window makeKeyAndVisible];
     return YES;
 }
